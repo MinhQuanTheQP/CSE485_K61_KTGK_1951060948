0a5b8abc531d355c2712fa9b1e74077f20874e0e
Quân Phạm <minhquanpro2001@gmail.com>
2021-09-20 21:56:06 +0700

    First commit

---
+++ BTVN1/Home/Home.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Home.css">
+
+
+</head>
+<body>
+    <div class="header">
+        <div class="header-logo">
+            <img src="images/logo.png"/>
+        </div>
+        <div class="header-nav">
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Categories</a>
+
+                </li>
+                <li>
+                    <a href="#">Foods</a>
+
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>           
+            </ul>
+        </div>
+    </div>
+    <div class="body">
+        <div class="search">
+            <div class="search-images">
+                <img src="images/background.jpg" width="100%" height="500px"/>
+            </div>
+            <div class="search-field">
+                <input type="text" placeholder="Search for food ..." />
+                <button>Search</button>
+            </div>
+        </div>
+        <div class="explore">
+            <div class="explore-title">
+                <h2>Explore Foods</h2>
+            </div>
+            <div class="explore-item">
+                <div class="explore-itemCard">
+                    <img src="images/pizza.jpg" alt="" width="100%" height="100%">
+                    <div class="explore-itemCard-title">
+                        <p>Pizza</p>
+                    </div>
+                </div>
+
+                <div class="explore-itemCard">
+                    <img src="images/burger.jpg" alt="" width="100%" height="100%">
+                    <div class="explore-itemCard-title">
+                        <p>Burger</p>
+                    </div>
+                </div>
+
+                <div class="explore-itemCard">
+                    <img src="images/momo.jpg" alt="" width="100%" height="100%">
+                    <div class="explore-itemCard-title">
+                        <p>Momos</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="menu">
+            <div class="menu-heading">
+                <h3>
+                    Food Menu
+                </h3>
+            </div>
+            <div class="menu-item">
+                <div class="menu-itemCard">
+                    <div class="menu-itemCard__img">
+                        <img src="images/menu-pizza.jpg" width="100%" height="100%" />
+                    </div>
+                    <div class="menu-itemCard__details">
+                        <h4>
+                            Food title
+                        </h4>
+                        <h5>$2.3</h5>
+                        <p>Made with Italian Sauce, Chicken, and organice vegetables.</p>
+                        <button>Order now</button>
+                    </div>
+                </div>
+
+                <div class="menu-itemCard">
+                    <div class="menu-itemCard__img">
+                        <img src="images/menu-burger.jpg" width="100%" height="100%" />
+                    </div>
+                    <div class="menu-itemCard__details">
+                        <h4>
+                            Smoky Burger
+                        </h4>
+                        <h5>$2.3</h5>
+                        <p>Made with Italian Sauce, Chicken, and organice vegetables.</p>
+                        <button>Order now</button>
+                    </div>
+                </div>
+
+                <div class="menu-itemCard">
+                    <div class="menu-itemCard__img">
+                        <img src="images/menu-pizza.jpg" width="100%" height="100%" />
+                    </div>
+                    <div class="menu-itemCard__details">
+                        <h4>
+                            Nice Burger
+                        </h4>
+                        <h5>$2.3</h5>
+                        <p>Made with Italian Sauce, Chicken, and organice vegetables.</p>
+                        <button>Order now</button>
+                    </div>
+                </div>
+
+                <div class="menu-itemCard">
+                    <div class="menu-itemCard__img">
+                        <img src="images/menu-burger.jpg" width="100%" height="100%" />
+                    </div>
+                    <div class="menu-itemCard__details">
+                        <h4>
+                            Smoky Burger
+                        </h4>
+                        <h5>$2.3</h5>
+                        <p>Made with Italian Sauce, Chicken, and organice vegetables.</p>
+                        <button>Order now</button>
+                    </div>
+                </div>
+
+                <div class="menu-itemCard">
+                    <div class="menu-itemCard__img">
+                        <img src="images/menu-pizza.jpg" width="100%" height="100%" />
+                    </div>
+                    <div class="menu-itemCard__details">
+                        <h4>
+                            Food title
+                        </h4>
+                        <h5>$2.3</h5>
+                        <p>Made with Italian Sauce, Chicken, and organice vegetables.</p>
+                        <button>Order now</button>
+                    </div>
+                </div>
+
+                <div class="menu-itemCard">
+                    <div class="menu-itemCard__img">
+                        <img src="images/menu-momo.jpg" width="100%" height="100%" />
+                    </div>
+                    <div class="menu-itemCard__details">
+                        <h4>
+                            Chicken Steam Momo
+                        </h4>
+                        <h5>$2.3</h5>
+                        <p>Made with Italian Sauce, Chicken, and organice vegetables.</p>
+                        <button>Order now</button>
+                    </div>
+                </div>
+
+                
+            </div>
+            <div class="menu-item__showMore">
+                <a href="#">See All Foods</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer">
+        <div class="socialMedia">
+            <div class="socialMedia-icon">
+                <a href="https://www.facebook.com/MinhQuanTheQP/" target="_blank"> 
+                    <i class="fab fa-facebook"></i>
+                    <img src="images/facebook-logo.png" width="100%" height="100%"/>
+                </a>
+            </div>
+            <div class="socialMedia-icon">
+                <a href="#">
+                    <i class="fab fa-instagram-square"></i>
+                    <img src="images/instagram.png" width="100%" height="100%"/>
+
+                </a>
+            </div>
+            <div class="socialMedia-icon">
+                <a href="#">
+                <i class="fab fa-twitter"></i>
+                <img src="images/twitter.png" width="100%" height="100%"/>
+                </a>
+            </div>
+        </div>
+        <div class="footer-desc">
+            All right reserved Designed By 
+            <span>
+                <a href="https://github.com/MinhQuanTheQP" target="_blank">MinhQuanTheQP</a>
+            </span>
+        </div>
+    </div>
+</body>
+</html>
---
+++ BTVN1/Order/Order.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Order.css">
+
+</head>
+<body>
+    <div class="hero-menu">
+        <header class="category-menu">
+            <img src="images/logo.png" alt="" class="category-logo">
+            <ul class="category-list-menu">
+                <li class="category-menu-item">
+                    <a href="" class="category-link">
+                        Home
+                    </a>
+                </li>
+                <li class="category-menu-item">
+                    <a href="" class="category-link">
+                        Category
+                    </a>
+                </li>
+                <li class="category-menu-item">
+                    <a href="" class="category-link">
+                        Foods
+                    </a>
+                </li>
+                <li class="category-menu-item">
+                    <a href="" class="category-link">
+                        Contact
+                    </a>
+                </li>
+            </ul>
+        </header>
+    </div>
+    <div class="order-form">
+        <h3 class="global-title">Fill this form to confirm your order</h3>
+        <div class="form-top">
+            <div class="top-left">
+                <img src="images/menu-pizza.jpg" alt="" class="left-img">
+            </div>
+            <div class="top-right">
+                <h3 class="right-title">Food title</h3>
+                <span class="right-price">$2.3</span>
+                <span class="right-price">Quantity</span>
+                <div class="right-quantity">
+                    <input type="number" value="1" class="quantity-select" />
+                </div>
+            </div>
+        </div>
+        <div class="form-bottom">
+            <form action="" class="form-group">
+                <label for="">Full name</label>
+                <input placeholder="Enter your name..." type="text" class="form-input">
+                <label for="">Phone number</label>
+                <input placeholder="Enter your phone..." type="text" class="form-input">
+                <label for="">Email</label>
+                <input placeholder="Enter your email..." type="text" class="form-input">
+                <label for="">Address</label>
+                <textarea name="" placeholder="Enter your address..." id="" cols="30" rows="10"></textarea>
+                <button>Confirm order</button>
+            </form>
+        </div>
+    </div>
+    <div class="footer-icon">
+        <img src="images/facebook-logo.png" alt="">
+        <img src="images/instagram.png" alt="">
+        <img src="images/twitter.png" alt="">
+    </div>
+    <div class="footer-desc">
+    All right reserved Designed By 
+            <span>
+                <a href="https://github.com/MinhQuanTheQP" target="_blank">MinhQuanTheQP</a>
+            </span>
+    </div>
+</body>
+</html>
