2bf584fc11677bf5851836d3867d0043e7a8defd
Quân Phạm <minhquanpro2001@gmail.com>
2021-10-22 09:27:19 +0700

    Baikiemtragiuaky

--- Hethongquanlynganhangmau/constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!isset($_SESSION)) {
-    session_start();
-}
-$siteurl = "http://localhost/MinhQuan/Hethongquanlynganhangmau/";
-$conn = mysqli_connect('localhost','root','','he_thong_quan_ly_ngan_hang_mau');
-    if(!$conn){
-        die("Không thể kết nối");
-    }
